c4619eb2c38d4ee9727a728d9d35a79238275bbc
varunyk1805 <varunyk1805@gmail.com>
2021-11-05 13:19:39 +0200

    add completed task-08

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,27 @@
+const loginForm = document.querySelector('.login-form');
 
+loginForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const {
+        elements: { email, password }
+    } = event.currentTarget;
+    
+    const userData = {
+        email,
+        password 
+    }
+
+    userData.email = email.value;
+    userData.password = password.value;
+
+    if (email.value.includes(' ') || password.value.includes(' ')) {
+        alert('Введені дані не повинні містити пробіли')
+    }
+    else if (email.value.length !== 0 && password.value.length !== 0) {
+        console.log(userData);
+        email.value = '';
+        password.value = '';
+    } else {
+        alert('Всі поля форми повинні бути заповнені');
+    }
+})
