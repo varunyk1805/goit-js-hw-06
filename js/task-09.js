7f96d061d88561b3b46bfef346a91e24f20f07a7
varunyk1805 <varunyk1805@gmail.com>
2021-11-05 13:34:11 +0200

    add completed task-09

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,12 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const backgroundColor = document.querySelector('body');
+let nameColor = document.querySelector('.color')
+
+const changeColorBtn = document.querySelector('.change-color');
+changeColorBtn.addEventListener('click', () => {
+  backgroundColor.style.backgroundColor = getRandomHexColor();
+  nameColor.textContent = getRandomHexColor();
+})
