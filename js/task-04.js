19b9c1f74376c629be0cda3f307c06813f4e5c3f
varunyk1805 <varunyk1805@gmail.com>
2021-11-14 01:49:57 +0200

    Update task-04.js

--- js/task-04.js
+++ js/task-04.js
@@ -5,12 +5,12 @@ value.textContent = counterValue;
 const decrement = () => {
     counterValue -= 1;
     return value.textContent = counterValue;
-}
+};
 
 const increment = () => {
     counterValue += 1;
     return value.textContent = counterValue;
-}
+};
 
 const decrementBtn = document.querySelector('button[data-action="decrement"]');
 decrementBtn.addEventListener('click', decrement);
