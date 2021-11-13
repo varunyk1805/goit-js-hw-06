a22b9e68462a7adc7eb9146d3ae3eacd0401f28f
varunyk1805 <varunyk1805@gmail.com>
2021-11-14 01:51:12 +0200

    Update task-07.js

--- js/task-07.js
+++ js/task-07.js
@@ -1,9 +1,8 @@
 const text = document.querySelector('#text');
 const fontSizeControl = document.querySelector('#font-size-control');
 
-fontSizeControl.setAttribute('value', '16')
+fontSizeControl.setAttribute('value', '16');
 
-const changeFontSize = (event) => text.style.fontSize = event.currentTarget.value + 'px';    
+const changeFontSize = event => text.style.fontSize = event.currentTarget.value + 'px';    
     
 fontSizeControl.addEventListener('input', changeFontSize);
-
